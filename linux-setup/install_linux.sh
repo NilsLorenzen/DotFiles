beba4a5a305fbc0650b5a40e1f4f8c431bf11812
Nils Lorenzen <mail@nilslorenzen.de>
2025-03-05 18:55:59 +0100

    add missing zsh theme to linux setup

--- linux-setup/install_linux.sh
+++ linux-setup/install_linux.sh
@@ -10,6 +10,9 @@ sudo apt install zsh -y
 echo "Install Oh My Zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   echo "Installiere Oh My Zsh..."
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
